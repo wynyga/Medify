2994b1f100ce4f9d8842985f1efe94660f45d82c
Can <wayanyoga32@gmail.com>
2025-11-22 22:57:42 +0800

    .

--- app/Http/Controllers/MasterItemsController.php
+++ app/Http/Controllers/MasterItemsController.php
@@ -23,8 +23,8 @@ class MasterItemsController extends Controller
 
         if (!empty($kode)) $data_search = $data_search->where('kode', $kode);
         if (!empty($nama)) $data_search = $data_search->where('nama', 'LIKE', '%' . $nama . '%');
-        if (!empty($hargamin)) $data_search = $data_search->where('harga_beli', '>=', $hargamin)->where('harga_beli', '<=', $hargamax);
-
+        if (!empty($hargamin)) $data_search = $data_search->where('harga_beli', '>=', $hargamin);
+        if (!empty($hargamax)) $data_search = $data_search->where('harga_beli', '<=', $hargamax);
         $data_search = $data_search->select('kode', 'nama', 'jenis', 'harga_beli', 'laba', 'supplier')->orderBy('id')->get();
 
 
@@ -59,7 +59,7 @@ class MasterItemsController extends Controller
             $kode = MasterItem::count('id');
             $kode = $kode + 1;
             $kode = str_pad($kode, 5, '0', STR_PAD_LEFT);
-            sleep(3);
+            // sleep(3);                                                                                                                                                                                                                                   
         } else {
             $data_item = MasterItem::find($id);
             $kode = $data_item->kode;
